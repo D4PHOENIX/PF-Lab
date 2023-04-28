3d946f862ba504d1670203f9f1b3864c207892c2
Daud Noman <daudnoman0@gmail.com>
2023-04-28 14:00:57 +0500

    achhaa

--- Just_Practice/1.cpp
+++ Just_Practice/1.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int rows = 6;
-    int i,j,n=1;
-    for (i=0; i<rows;i++){
-        for (j=0; j<=i;j++){
-            cout<<n<<" ";
-            n++;
-            }
-    cout<<endl;
+void prime(int num){
+    bool isPrime = true;
+    if (num<2){
+        isPrime = false;
     }
+    else{
+        for(int i = 2; i<=num/2; ++i){
+            if (num%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+    }
+    if (isPrime){
+        cout<<"Entered number is a prime number.";
+    }
+    else{
+        cout<<"Entered number is not a prime number.";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    prime(n);
+
 return 0;    
 }
