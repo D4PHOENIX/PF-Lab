3611e035a96e4ba954b0a6f0592a0c69460ac5da
Daud Noman <daudnoman0@gmail.com>
2023-05-08 20:07:26 +0500

    checkinnn

--- Just_Practice/2.cpp
+++ Just_Practice/2.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main(){
-    // int matrix[3][3];
-    // int i,j;
-    // for (i=0; i<3;i++){
-    //     for (j=0; j<3;j++){
-    //         cout<<"Enter values for the matrix: ";
-    //         cin>>matrix[i][j];
-    //     }     
-    // }
-    // cout<<"\n\n";
-    // for (i=0;i<3;i++){
-    //     for (j=0;j<3;j++){
-    //         cout<<matrix[i][j]<<"\t";lv                       
+    int matrix[3][3];
+    int i,j;
+    for (i=0; i<3;i++){
+        for (j=0; j<3;j++){
+            cout<<"Enter values for the matrix: ";
+            cin>>matrix[i][j];
+        }     
+    }
+    cout<<"\n\n";
+    for (i=0;i<3;i++){
+        for (j=0;j<3;j++){
+            cout<<matrix[i][j]<<"\t";lv                       
         
-    //     }
-    //     cout<<"\n";
-    // }
+        }
+        cout<<"\n";
+    }
     int i, n;
     for(i=0;i<=4;i++){
         for(i=n;i<=n;n++){
